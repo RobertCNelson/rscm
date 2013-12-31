7acfdcc6b3ae7073e6f2219d33a5a57c838b0f84
Robert Nelson <robertcnelson@gmail.com>
2013-12-31 09:18:44 -0600

    move from patches to simple, makes room for complex ones..
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- 3.13.0-rc6-bone2/rebase.sh
+++ 3.13.0-rc6-bone2/rebase.sh
@@ -6,14 +6,14 @@ rebase () {
 	patch -p2 -R < ./${patch_file}
 }
 
-patch_file="patches/enable-ttyO1.diff"
+patch_file="simple/enable-ttyO1.diff"
 rebase
 
-patch_file="patches/enable-ttyO2.diff"
+patch_file="simple/enable-ttyO2.diff"
 rebase
 
-patch_file="patches/enable-ttyO4.diff"
+patch_file="simple/enable-ttyO4.diff"
 rebase
 
-patch_file="patches/enable-ttyO5.diff"
+patch_file="simple/enable-ttyO5.diff"
 rebase
